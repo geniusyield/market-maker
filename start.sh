194d99f86ffcb23f52c6bed39df99cc092fed9bb
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2024-01-02 10:53:06 +0100

    Docker Compose#6

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ls -l ~/
-echo "$PROVIDER_CONFIG" >> ~/config.json
-echo "$MARKET_MAKER_CONFIG" >> ~/marker-maker-config.json
-echo "$PAYMENT_SIGNING_KEY" >> ~/payment-signing-key.skey
+echo $PROVIDER_CONFIG >> ~/config.json
+echo $MARKET_MAKER_CONFIG >> ~/marker-maker-config.json
+echo $PAYMENT_SIGNING_KEY >> ~/payment-signing-key.skey
 cabal run geniusyield-market-maker-exe -- Run ~/config.json ~/market-maker-config.json

afd40c5ee70c00e90d9a317095fb97260970ef1f
sourabhxyz <swasti@sourabh.xyz>
2024-01-05 20:55:51 +0530

    Feat #11 and rest, additional comment for percent fee constant

--- geniusyield-market-maker/src/GeniusYield/MarketMaker/Constants.hs
+++ geniusyield-market-maker/src/GeniusYield/MarketMaker/Constants.hs
@@ -13,6 +13,6 @@ awaitTxParams = GYAwaitTxParameters {maxAttempts = 20, confirmations = 1, checkI
 logNS ∷ GYLogNamespace
 logNS = "MM"
 
--- TODO: Get it from blockchain instead.
+-- TODO: Get it from blockchain instead. Note that this is only used to determine funds needed by wallet and is not forwarded to dex-contracts-api library.
 makerFeeRatio ∷ Rational
 makerFeeRatio = 3 % 1000 -- Is 0.3%, so ratio should be 0.003 == 3 / 1000.

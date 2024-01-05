c3ac950e7b07bf53cd5ed293db1a0c609f03a286
Lars Brünjes <brunjlar@gmail.com>
2024-01-05 23:28:09 +0100

    implemented issue #27

--- geniusyield-market-maker/src/GeniusYield/MarketMaker/MakerBot.hs
+++ geniusyield-market-maker/src/GeniusYield/MarketMaker/MakerBot.hs
@@ -1,56 +1,52 @@
 module GeniusYield.MarketMaker.MakerBot where
 
-import Control.Concurrent (threadDelay)
-import Control.Exception (Exception (displayException), Handler (Handler), catches)
-import Control.Monad (forM_, forever, when)
-import Control.Monad.Reader (runReaderT)
-import Data.List.Split (chunksOf)
-import qualified Data.Map.Strict as M
-import GeniusYield.Api.Dex.PartialOrder (
-  PartialOrderInfo (poiOwnerKey),
-  cancelMultiplePartialOrders,
-  partialOrders,
-  placePartialOrder,
- )
-import GeniusYield.Imports (printf)
-import GeniusYield.MarketMaker.Constants (awaitTxParams, logNS)
-import GeniusYield.MarketMaker.Prices
-import GeniusYield.MarketMaker.Strategies
-import GeniusYield.MarketMaker.User
-import GeniusYield.MarketMaker.Utils (
-  DEXInfo (dexPORefs),
-  addrFromSkey,
-  pkhFromSkey,
- )
-import GeniusYield.Providers.Common (SubmitTxException)
-import GeniusYield.Transaction (BuildTxException)
-import GeniusYield.TxBuilder
-import GeniusYield.Types
-import System.Exit
+import           Control.Concurrent                 (threadDelay)
+import           Control.Exception                  (Exception (displayException),
+                                                     Handler (Handler), catches)
+import           Control.Monad                      (forM_, forever, when)
+import           Control.Monad.Reader               (runReaderT)
+import           Data.List.Split                    (chunksOf)
+import qualified Data.Map.Strict                    as M
+import           GeniusYield.Api.Dex.PartialOrder   (PartialOrderInfo (poiOwnerKey),
+                                                     cancelMultiplePartialOrders,
+                                                     partialOrders,
+                                                     placePartialOrder)
+import           GeniusYield.Imports                (printf)
+import           GeniusYield.MarketMaker.Constants  (awaitTxParams, logNS)
+import           GeniusYield.MarketMaker.Prices
+import           GeniusYield.MarketMaker.Strategies
+import           GeniusYield.MarketMaker.User
+import           GeniusYield.MarketMaker.Utils      (DEXInfo (dexPORefs),
+                                                     addrUser, pkhUser)
+import           GeniusYield.Providers.Common       (SubmitTxException)
+import           GeniusYield.Transaction            (BuildTxException)
+import           GeniusYield.TxBuilder
+import           GeniusYield.Types
+import           System.Exit
 
 data MakerBot = MakerBot
   { -- | User.
-    mbUser ∷ User,
+    mbUser  :: User,
     -- | Delay in microseconds between each iteration of execution strategy loop.
-    mbDelay ∷ Int,
+    mbDelay :: Int,
     -- | Non-ADA token as other pair of the token is assumed to be ADA.
-    mbToken ∷ MMToken
+    mbToken :: MMToken
   }
 
 -----------------------------------------------------------------------
 ---------------------------- ACTIONS ----------------------------------
 
 -- | Scan the chain for existing orders and cancel all of them in batches of 6.
-cancelAllOrders ∷ MakerBot → GYNetworkId → GYProviders → DEXInfo → IO ()
+cancelAllOrders :: MakerBot -> GYNetworkId -> GYProviders -> DEXInfo -> IO ()
 cancelAllOrders MakerBot {mbUser} netId providers di = do
-  let go ∷ [PartialOrderInfo] → IO ()
+  let go :: [PartialOrderInfo] -> IO ()
       go partialOrderInfos = do
         gyLogInfo providers logNS $ "---------- " ++ show (length partialOrderInfos) ++ " orders to cancel! -----------"
         when (null partialOrderInfos) $ do
           gyLogInfo providers logNS "---------- No more orders to cancel! -----------"
           exitSuccess
         let (batch, rest) = splitAt 6 partialOrderInfos
-            userAddr = addrFromSkey netId $ uSKey mbUser
+            userAddr = addrUser netId mbUser
         txBody ←
           runGYTxMonadNode netId providers [userAddr] userAddr (uColl mbUser)
             $ runReaderT (cancelMultiplePartialOrders (dexPORefs di) batch) di
@@ -61,23 +57,23 @@ cancelAllOrders MakerBot {mbUser} netId providers di = do
         gyLogInfo providers logNS "---------- Done for the block! -----------"
         gyAwaitTxConfirmed providers awaitTxParams tid
         go rest
-  partialOrderInfos ← runGYTxQueryMonadNode netId providers $ runReaderT (partialOrders (dexPORefs di)) di
-  let userPkh = pkhFromSkey . uSKey $ mbUser
-      userPOIs = filter (\o → poiOwnerKey o == userPkh) $ M.elems partialOrderInfos
+  partialOrderInfos <- runGYTxQueryMonadNode netId providers $ runReaderT (partialOrders (dexPORefs di)) di
+  let userPkh = pkhUser mbUser
+      userPOIs = filter (\o -> poiOwnerKey o == userPkh) $ M.elems partialOrderInfos
   go userPOIs
 
-buildAndSubmitActions ∷ User → GYProviders → GYNetworkId → UserActions → DEXInfo → IO ()
-buildAndSubmitActions User {uSKey, uColl, uStakeAddress} providers netId ua di = flip catches handlers $ do
-  let userAddr = addrFromSkey netId uSKey
+buildAndSubmitActions :: User -> GYProviders -> GYNetworkId -> UserActions -> DEXInfo -> IO ()
+buildAndSubmitActions user@User {uSKey, uColl, uStakeAddress} providers netId ua di = flip catches handlers $ do
+  let userAddr = addrUser netId user
       placeActions = uaPlaces ua
       cancelActions = uaCancels ua
 
-  forM_ (chunksOf 6 cancelActions) $ \cancelChunk → do
+  forM_ (chunksOf 6 cancelActions) $ \cancelChunk -> do
     logInfo $ "Building for cancel action(s): " <> show cancelChunk
     txBody ← runGYTxMonadNode netId providers [userAddr] userAddr uColl $ flip runReaderT di $ cancelMultiplePartialOrders (dexPORefs di) (map coaPoi cancelChunk)
     buildCommon txBody
 
-  forM_ placeActions $ \pa@PlaceOrderAction {..} → do
+  forM_ placeActions $ \pa@PlaceOrderAction {..} -> do
     logInfo $ "Building for place action: " <> show pa
     txBody ←
       runGYTxMonadNode netId providers [userAddr] userAddr uColl
@@ -97,16 +93,16 @@ buildAndSubmitActions User {uSKey, uColl, uStakeAddress} providers netId ua di =
   logInfo = gyLogInfo providers logNS
 
   handlers =
-    let handlerCommon ∷ Exception e ⇒ e → IO ()
+    let handlerCommon :: Exception e => e -> IO ()
         handlerCommon = logWarn . displayException
 
-        be ∷ BuildTxException → IO ()
+        be :: BuildTxException -> IO ()
         be = handlerCommon
 
-        se ∷ SubmitTxException → IO ()
+        se :: SubmitTxException -> IO ()
         se = handlerCommon
 
-        me ∷ GYTxMonadException → IO ()
+        me :: GYTxMonadException -> IO ()
         me = handlerCommon
      in [Handler be, Handler se, Handler me]
 
@@ -120,13 +116,13 @@ buildAndSubmitActions User {uSKey, uColl, uStakeAddress} providers netId ua di =
     logInfo $ printf "Tx successfully seen on chain with %d confirmation(s)" numConfirms
 
 executeStrategy
-  ∷ Strategy
-  → MakerBot
-  → GYNetworkId
-  → GYProviders
-  → PricesProviders
-  → DEXInfo
-  → IO ()
+  :: Strategy
+  -> MakerBot
+  -> GYNetworkId
+  -> GYProviders
+  -> PricesProviders
+  -> DEXInfo
+  -> IO ()
 executeStrategy runStrategy MakerBot {mbUser, mbDelay, mbToken} netId providers pp di =
   forever $ do
     newActions ← runStrategy pp mbUser mbToken
--- geniusyield-market-maker/src/GeniusYield/MarketMaker/User.hs
+++ geniusyield-market-maker/src/GeniusYield/MarketMaker/User.hs
@@ -1,46 +1,46 @@
 module GeniusYield.MarketMaker.User where
 
-import Data.Aeson (withText)
-import Deriving.Aeson
-import GeniusYield.Imports
-import GeniusYield.Types
+import           Data.Aeson          (withText)
+import           Deriving.Aeson
+import           GeniusYield.Imports
+import           GeniusYield.Types
 
 -- TODO: Move to Atlas.
 newtype GYStakeAddressBech32 = GYStakeAddressBech32 GYStakeAddress
   deriving newtype (Show, Eq, Ord)
 
-stakeAddressToBech32 ∷ GYStakeAddress → GYStakeAddressBech32
+stakeAddressToBech32 :: GYStakeAddress -> GYStakeAddressBech32
 stakeAddressToBech32 = coerce
 
-stakeAddressFromBech32 ∷ GYStakeAddressBech32 → GYStakeAddress
+stakeAddressFromBech32 :: GYStakeAddressBech32 -> GYStakeAddress
 stakeAddressFromBech32 = coerce
 
 instance ToJSON GYStakeAddressBech32 where
   toJSON (GYStakeAddressBech32 addr) = toJSON $ stakeAddressToText addr
 
 instance FromJSON GYStakeAddressBech32 where
-  parseJSON = withText "GYStakeAddressBech32" $ \t →
+  parseJSON = withText "GYStakeAddressBech32" $ \t ->
     case stakeAddressFromTextMaybe t of
-      Just addr → pure $ GYStakeAddressBech32 addr
-      Nothing → fail "cannot deserialise stake address"
+      Just addr -> pure $ GYStakeAddressBech32 addr
+      Nothing   -> fail "cannot deserialise stake address"
 
 data UserRaw = UserRaw
-  { urSKeyPath ∷ !FilePath,
-    urColl ∷ !(Maybe GYTxOutRef),
-    urStakeAddress ∷ !(Maybe GYStakeAddressBech32)
+  { urSKeyPath     :: !FilePath,
+    urColl         :: !(Maybe GYTxOutRef),
+    urStakeAddress :: !(Maybe GYStakeAddressBech32)
   }
   deriving stock (Generic, Show, Eq)
   deriving (FromJSON, ToJSON) via CustomJSON '[FieldLabelModifier '[CamelToSnake]] UserRaw
 
-getUser ∷ UserRaw → IO User
+getUser :: UserRaw -> IO User
 getUser UserRaw {urSKeyPath, urColl, urStakeAddress} = do
   let collateral = (,False) <$> urColl
   uSKey ← readPaymentSigningKey urSKeyPath
   pure $ User {uSKey = uSKey, uColl = collateral, uStakeAddress = urStakeAddress}
 
 data User = User
-  { uSKey ∷ !GYPaymentSigningKey,
-    uColl ∷ !(Maybe (GYTxOutRef, Bool)),
-    uStakeAddress ∷ !(Maybe GYStakeAddressBech32)
+  { uSKey         :: !GYPaymentSigningKey,
+    uColl         :: !(Maybe (GYTxOutRef, Bool)),
+    uStakeAddress :: !(Maybe GYStakeAddressBech32)
   }
   deriving stock (Generic, Show, Eq, Ord)
